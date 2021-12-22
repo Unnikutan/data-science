556bca05fc5f9798541555f9777674828bdb6128
Meeraprasad1104 <74696239+Meeraprasad1104@users.noreply.github.com>
2021-12-22 15:48:49 +0530

    Add files via upload

---
+++ Exercise_3/exc_3_5.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+plt.subplot(2,2,1)
+plt.subplot(2,2,2)
+plt.subplot(2,2,3)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
