c9840f7128a8549626ca7ca10d4a2793445e3a22
Meeraprasad1104 <74696239+Meeraprasad1104@users.noreply.github.com>
2021-12-25 12:28:54 +0530

    Update exc_3_6_2.py

--- Exercise_3/exc_3_6/exc_3_6_2.py
+++ Exercise_3/exc_3_6/exc_3_6_2.py
@@ -9,8 +9,8 @@ import numpy as np
 x = np.array(["java","python","php","javascript","c#","c++"])
 y = np.array([22.2,17.6,8.8,8,77,6.7])
 plt.barh(x,y,color="red")
-plt.xlabel("programming language")
-plt.ylabel("popularity")
+plt.ylabel("programming language")
+plt.xlabel("popularity")
 plt.show()
 
 
