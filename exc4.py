4c79c5805020eca718f6ddc8ccabde8e6d9a719e
Meeraprasad1104 <74696239+Meeraprasad1104@users.noreply.github.com>
2022-01-23 16:30:00 +0530

    Update exc4.py

--- exc4.py
+++ exc4.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[8]:
+# In[1]:
 
 
 import pandas as pd
@@ -9,7 +9,7 @@ x=pd.Series(['a','b','c'])
 print(x)
 
 
-# In[9]:
+# In[2]:
 
 
 from datetime import timedelta, date
@@ -24,7 +24,7 @@ for dt in daterange(start, end):
     print(dt.strftime("%Y-%m-%d"))
 
 
-# In[10]:
+# In[3]:
 
 
 import pandas as pd
@@ -38,7 +38,7 @@ df = pd.DataFrame(details)
 df
 
 
-# In[11]:
+# In[4]:
 
 
 import pandas as pd
@@ -49,7 +49,7 @@ df = pd.DataFrame.from_records(data)
 df
 
 
-# In[12]:
+# In[5]:
 
 
 import pandas as pd
@@ -57,7 +57,7 @@ df = pd.read_csv("data.csv")
 print(df.head())
 
 
-# In[28]:
+# In[6]:
 
 
 import numpy as np
@@ -71,7 +71,7 @@ print('\nSORTED DATAFRAME\n')
 print(df.sort_values(by=['Age'], ascending=True))
 
 
-# In[57]:
+# In[7]:
 
 
 import pandas as pd
@@ -85,7 +85,7 @@ df = pd.DataFrame(data, index)
 df
 
 
-# In[58]:
+# In[8]:
 
 
 import pandas as pd
@@ -101,8 +101,48 @@ df[0:2]
 #df1
 
 
-# In[ ]:
+# In[ 9]:
 
+import pandas  as pd
+data={'name':['meera','kavya'],
+      'occupation':['engineer','manager'],
+      'salary':[45000,55000]}
+df=pd.Dataframe(data)
+print(df)
+sal_avg=df.groupby('occupation')['salary'].mean()
+print(sal_avg)
 
+# In[10]:
 
+import pandas as pd
+df1=[[2,'anu',49],[4,'devika'],[8,'manu'],[10,'sona',50]]
+df2=Dataframe(df1,columnd=['Rollno','Name','Mark'])
+print(df2)
+print("\n after modification:\nafter modification:\n")
+df3=df2.fillna(0)
+print(df3)
+
+# In[11]:
+
+import pandas as pd
+df11=pd.read.csv("program11.csv")
+print(df1)
+df1[profit]=df1['profit']apply(lamda x:x>0)
+df1
+
+# In[12]:
+
+import  pandas as pd
+l1=[[100,'Arun',10000],[200,'vivek',14000],[300,'sona',9000]]
+df1=pd.Dataframe(l1.columns=['eid','ename','stipend'])
+print("\n Dataframe\n")
+print(df1)
+l2=[[100,'teacher'],[200,'hod'],[300,'librarian']]
+df2=pd.ataframe(l2.columns=['eid','ename','stipend'])
+print("\n Dataframe\n")
+print(df2)
+l3=pd.merge(df1,df2,how="inner",own="eid")
+l3=rename(column2={'designation':'position'},inplace=True)
+print("merged dataframe")
+l3
 
